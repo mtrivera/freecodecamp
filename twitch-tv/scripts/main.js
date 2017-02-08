516507a0e33edce7dc7225c3537821037fab591d
Miguel T Rivera <mrivera108@outlook.com>
2017-02-07 22:06:51 -0800

    Disabled module & created Promise function

--- twitch-tv/scripts/main.js
+++ twitch-tv/scripts/main.js
@@ -1,73 +1,98 @@
+/*promise get implementation
+polyfills for ie8+
+https://github.com/taylorhakes/promise-polyfill
+https://github.com/stefanpenner/es6-promise
+https://developers.google.com/web/fundamentals/getting-started/primers/promises
+*/
+function get(url) {
+  // Return a new promise
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    var req = new XMLHttpRequest();
+    req.open('GET', url, true);
+
+    req.onload = function() {
+      // This is called even on 404 etc
+      // so check the status
+      if (req.status == 200) {
+        resolve(req.response);
+      } else {
+        reject(Error(req.statusTxt));
+      }
+    };
+
+    // Handle network errors
+    req.onerror = function() {
+      reject(Error('Network Error'));
+    };
+
+    // Make the request
+    req.send();
+  });
+}
+/*
 function ajaxModule() {
-  //to see if a streamer is offline
+  //to see if a streamer is offline and get data
   function findStream(stream) {
     const API_URL = 'https://wind-bow.gomix.me/twitch-api';
     let request = new XMLHttpRequest();
     request.open('GET', API_URL + '/streams/' + stream, true);
-    request.onload = function() {
+    request.onload = function streamDataParse() {
       var data = JSON.parse(this.response);
        isStreamOnline(data, stream);
       }
     request.send();
   }
+
   //to see if a valid user
   function findUser(user) {
+    var streamer = {};
     const API_URL = 'https://wind-bow.gomix.me/twitch-api';
     let request = new XMLHttpRequest();
     request.open('GET', API_URL + '/users/' + user, true);
-    request.onload = function() {
+    request.onload = function userDataParse() {
       var data = JSON.parse(this.response);
-        isValidUser(data);
-      }
-    request.send();
-  }
-  //to get data to populate list
-  function findChannel(channel) {
-    const API_URL = 'https://wind-bow.gomix.me/twitch-api';
-    let request = new XMLHttpRequest();
-    request.open('GET', API_URL + '/channels/' + channel, true);
-    request.onload = function() {
-      var data = JSON.parse(this.response);
-        displayChannelContent(data);
-      }
+      isUserValid(data, user);
+    }
     request.send();
   }
 
-    return {
+  return {
     findStream: findStream,
-    findUser: findUser,
-    findChannel: findChannel
+    findUser: findUser
   };
 }
-
+*/
 //use with findUser
-function isValidUser(user) {
-  const NOT_FOUND = 404,
-        UNPROCESSABLE = 422;
-  if (user.status !== NOT_FOUND && user.status !== UNPROCESSABLE) {
-    isStreamOnline(user);
-  } else {
-    var msg = document.createElement('p');
-    var msgText = document.createTextNode(user.message);
-    msg.appendChild(msgText);
-    document.body.appendChild(msg);
-  }
-}
+function isUserValid(data, user) {
+  const NOT_FOUND = 404;
+  const UNPROCESSABLE = 422;
+  var streamer = {};
 
-//use with findChannel
-function displayChannelContent(channel) {
-    addLogo(channel.logo, channel.name, channel.status);  /*+ '<br />' +
-    channel.name + '<br / >' + channel.status + '<br / >' + channel.url +
-    '<br />' + channel.game;*/
+  if (data.status !== NOT_FOUND && data.status !== UNPROCESSABLE) {
+    streamer.name = data.name;
+    streamer.account_status = true;
+  } else {
+    streamer.name = user;
+    streamer.account_status = false;
+  }
+  return streamer;
 }
 
 //to see if channel is online
 function isStreamOnline(data, stream) {
   const netStatus = document.createElement('p');
   var netStatusTxt;
+  var statusP;
   if (data.stream !== null) {
+    addLogo(data.stream.channel.logo, data.stream.channel.name, data.stream.channel.status);
     netStatusTxt = document.createTextNode(data.stream.channel.name +
     ' is ONLINE');
+    addElm(statusP, data.stream.channel.status, 'p');
+    /*var statusP = document.createElement('p');
+    var statusPText = document.createTextNode(data.stream.channel.status);
+    statusP.appendChild(statusPText);
+    document.body.appendChild(statusP);*/
   } else {
     netStatusTxt = document.createTextNode(stream + ' is OFFLINE');
   }
@@ -84,6 +109,13 @@ function addLogo(url, stream, status) {
   document.body.appendChild(logoImage);
 }
 
+function addElm(name, data, tag) {
+  var name = document.createElement(tag);
+  var nameText = document.createTextNode(data);
+  name.appendChild(nameText);
+  document.body.appendChild(name);
+}
+
 //use for creating list
 function addListItem(streamer, netStatus) {
   var listItem = document.createElement('li');
@@ -98,19 +130,16 @@ function addListItem(streamer, netStatus) {
   var ul = document.createElement('ul');
   ul.setAttribute('class', 'streamerList');
 
-  var t;
-  var streamers = ["ESL_DOTA2", "BeyondTheSummit", "cretetion",
+  var users = ["ESL_DOTA2", "BeyondTheSummit", "cretetion",
   "comster404", "freecodecamp", "storbeck", "brunofin", "habathcx",
   "RobotCaleb", "noobs2ninjas"];
 
   //document.getElementById('contentList').appendChild('ul');
 
-  for(let count = 0; count < streamers.length; count += 1) {
-    xhr.findUser(streamers[count]);
+    //console.log(streamers);
     /*var li = document.createElement('li');
     li.setAttribute('class', 'streamer');
     ul.appendChild('li');
     t = document.createTextNode(element);
     li.innerHTML = li.innerHTML + element;*/
-  }
 }());
