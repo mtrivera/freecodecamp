f913345e7de93c4331406882df837939064e71b7
Miguel T Rivera <mrivera108@outlook.com>
2017-02-12 02:43:04 -0800

    Refactor isUserValid() & isStreamOnline()

--- twitch-tv/scripts/main.js
+++ twitch-tv/scripts/main.js
@@ -36,17 +36,22 @@ function getJSON(url) {
 }
 
 // use with findUser
-function isUserValid(data, user) {
+function isUserValid(data) {
   const NOT_FOUND = 404;
   const UNPROCESSABLE = 422;
   var streamer = {};
 
   if (data.status !== NOT_FOUND && data.status !== UNPROCESSABLE) {
-    streamer.name = data.name;
-    streamer.account_status = true;
+    const account_status = true;
+    streamer = {
+      account_status
+    };
   } else {
-    streamer.name = user;
-    streamer.account_status = false;
+    const errorMsg = data.message;
+    const account_status = false;
+    streamer = {
+      errorMsg, account_status
+    };
   }
   return streamer;
 }
@@ -54,16 +59,26 @@ function isUserValid(data, user) {
 // to see if channel is online
 function isStreamOnline(data, stream) {
   var streamer = {};
+
   if (data.stream === null) {
-    streamer.network_status = false;
+    const name = stream;
+    const network_status = false;
+    streamer = {
+      name, network_status
+    };
   } else {
-    streamer.game = data.stream.game;
-    streamer.viewers = data.stream.viewers;
-    streamer.mature = data.stream.channel.mature;
-    streamer.logo_url = data.stream.channel.logo;
-    streamer.stream_status = data.stream.channel.status;
-    streamer.stream_url = data.stream.channel.url;
-    streamer.network_status = true;
+    const name = data.stream.channel.name;
+    const game = data.stream.game;
+    const viewers = data.stream.viewers;
+    const mature =  data.stream.channel.mature;
+    const logo_url = data.stream.channel.logo;
+    const stream_status = data.stream.channel.status;
+    const stream_url =  data.stream.channel.url;
+    const network_status = true;
+
+    streamer = { name, game, viewers, mature, logo_url,
+      stream_status, stream_url, network_status
+    };
   }
   return streamer;
 }
@@ -98,25 +113,26 @@ function addListItem(streamer, netStatus) {
 
   var ul = document.createElement('ul');
   ul.setAttribute('class', 'streamerList');
-  const dota = 'comster404';
-  var users = ['ESL_DOTA2', 'BeyondTheSummit', 'cretetion',
-  'comster404', 'freecodecamp', 'storbeck', 'brunofin', 'habathcx',
+  const testUser = 'baduser404';
+  var users = ['ESL_DOTA2', 'BeyondTheSummit', 'zai',
+  'baduser404', 'freecodecamp', 'storbeck', 'brunofin', 'habathcx',
   'RobotCaleb', 'noobs2ninjas'];
   // Promise calls
   const userPromise = getJSON(`${API_URL}/users/${dota}`);
   const streamPromise = getJSON(`${API_URL}/streams/${dota}`);
 
   Promise.all([userPromise, streamPromise]).then((data) => {
-    const userData = isUserValid(data[0], dota);
+    const userData = isUserValid(data[0]);
   // TODO: Work on promise logic, ask why spread operator does not work
     if (!userData.account_status) {
-      console.log(`${userData.name} Does Not Exist`);
+      console.log(userData.errorMsg);
     } else {
       const streamerData = isStreamOnline(data[1], dota);
       if(!streamerData.network_status) {
-        console.log('User exists but is currently offline');
+        //console.log(`${userData.name} is currently OFFLINE`);
       } else {
         console.log('User Exists! Display Streamer Data');
+        addLogo(streamerData.logo_url, streamerData.name, streamerData.stream_status);
         console.log(streamerData);
       }
     }
