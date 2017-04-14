74acead347d2a135341552d8af8ff4226dc21f69
Miguel T Rivera <mrivera108@outlook.com>
2017-04-13 20:29:30 -0700

    Make the calculator work

--- calculator/scripts/app.js
+++ calculator/scripts/app.js
@@ -9,16 +9,36 @@ function ready(fn) {
 }
 
 function getInputData() {
-    // Event Delegation
+    var vals = [];
+    var equation = document.getElementById('equation');
     var form = document.getElementById('calculator');
-    form.addEventListener('click', function(e) {
+    form.addEventListener('click', function calculate(e) {
     // e.target is the clicked element!!
-    // If it was an input button
-    if (e.target && e.target.nodeName === 'INPUT') {
+    // Event delegation
+      if (e.target && e.target.nodeName === 'INPUT') {
     // List the button name and its value
-        console.log(`Button ${e.target.id} was clicked! Value: ${e.target.value}`);
-    }
-  });
+        //console.log(`Button ${e.target.id} was clicked! Value: ${e.target.value}`);
+        // TODO: Use switch statement
+          if (e.target.value === 'AC') {
+            equation.value = '';
+            vals = [];
+          } else if (e.target.value === 'DEL') {
+            vals.pop();
+            equation.value = vals.join('');
+          } else if (e.target.value === '÷') {
+            equation.value += '/';
+            vals.push('/');
+          } else if (e.target.value === '×') {
+            equation.value += '*';
+            vals.push('*');
+          } else if (e.target.value !== '=') {
+            equation.value += e.target.value;
+            vals.push(e.target.value);
+          } else {
+            equation.value = eval(equation.value);
+          }
+      }
+    });
 }
 
 ready(getInputData);
