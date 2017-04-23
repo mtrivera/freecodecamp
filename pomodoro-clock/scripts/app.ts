4e671780db57245c794c014708bda095c319db27
Miguel T Rivera <mrivera108@outlook.com>
2017-04-22 23:58:26 -0700

    Working countdown

--- pomodoro-clock/scripts/app.ts
+++ pomodoro-clock/scripts/app.ts
@@ -1,14 +1,73 @@
 'use strict';
 
 let session:any = document.getElementById('session');
-let count:number = 0;
 let minutes:number = 25;
-let seconds:number = 59;
+let seconds:number = 0;
+let zeroSet:string = '00';
+
 let interval = setInterval(function() {
-  if (count > minutes) {
+  stopClock(minutes);
+
+  if (seconds < 0) {
+    setClock(minutes);
+  }
+
+  session.textContent = `${minutes}:${seconds}`;
+  appendZero(minutes, seconds);
+/*
+  appendMinuteZero(minutes);
+  appendSecondZero(seconds);
+*/
+  if (seconds > 0 ) {
+    seconds -= 1;
+  } else {
+    seconds = 59;
+    minutes -= 1;
+  }
+
+  if (minutes < 0) {
+    zeroClock(minutes, seconds);
+  }
+}, 1000);
+
+function zeroClock(minutes: number, seconds: number) {
+  return `${zeroSet}:${zeroSet}`;
+}
+
+function setClock(minutes: number) {
+  return `${minutes}:${zeroSet}`;
+}
+
+function stopClock(minutes: number) {
+  // Session is over, stop timer
+  if (minutes < 0) {
     clearInterval(interval);
   }
-  session.textContent = `${minutes}:${seconds}`;
-  minutes--;
-  count += 1;
-}, 1000);
+}
+// TODO: If the minute is < 10, the zero is not appended correctly for seconds
+function appendZero(minutes: number, seconds: number) {
+  if (minutes < 10 ) {
+    // Append leading zero to minutes < 10
+    session.textContent =  `0${minutes}:${seconds}`;
+  } else if (seconds < 10) {
+    // Append leading zero to minutes < 10
+    session.textContent =  `${minutes}:0${seconds}`;
+  } else if (seconds < 10 && minutes < 10) {
+    session.textContent =  `0${minutes}:0${seconds}`;
+  }
+}
+/*
+function appendMinuteZero(minutes: number) {
+  if (minutes < 10) {
+    // Append leading zero to minutes < 10
+    session.textContent = `0${minutes}:${seconds}`;
+  }
+}
+
+function appendSecondZero(seconds: number) {
+  if (seconds < 10) {
+    // Append leading zero to minutes < 10
+    session.textContent = `${minutes}:0${seconds}`;
+  }
+}
+*/
