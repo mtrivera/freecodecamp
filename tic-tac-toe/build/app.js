cd0a6b5e6e4d63e1dab74e41c25d959f6df976bc
Miguel T Rivera <mrivera108@outlook.com>
2017-05-18 21:52:55 -0700

    Implement minimax algorithm

--- tic-tac-toe/build/app.js
+++ tic-tac-toe/build/app.js
@@ -1,25 +1,95 @@
-let Player = {
-    marker: '',
-    score: 0,
-    winsTotal: 0
-};
 let origBoard = ['O', 1, 'X', 'X', 4, 'X', 6, 'O', 'O'];
-// Possible winning combinations
-const threeAcross = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
-const threeDown = [[0, 3, 6], [1, 4, 7], [2, 5, 8]];
-const threeDiagonal = [[0, 4, 8], [2, 4, 6]];
-let human = Object.create(Player);
-human.marker = 'O';
-let ai = Object.create(Player);
-ai.marker = "X";
-// TODO: Build this function out
-function miniMax(newBoard, player) {
+let human = {
+    marker: 'O',
+    winsTotal: undefined
+};
+let ai = {
+    marker: 'X',
+    winsTotal: undefined
+};
+/* TEST
+let bestSpot = minimax(origBoard, ai.marker);
+console.log("index: " + bestSpot.index);  // 4
+*/
+function minimax(newBoard, player) {
+    let availableSpots = emptyIndices(newBoard);
+    //check for terminal state
+    if (winningCombo(newBoard, human.marker)) {
+        return { score: -10 };
+    }
+    else if (winningCombo(newBoard, ai.marker)) {
+        return { score: 10 };
+    }
+    else if (availableSpots.length === 0) {
+        return { score: 0 };
+    }
+    // An array to collect all the objects
+    let moves = [];
+    // Loop through available spots
+    for (let count = 0; count < availableSpots.length; count += 1) {
+        // Create an object for each and store the index of that spot
+        let move = {
+            index: undefined,
+            score: undefined
+        };
+        move.index = newBoard[availableSpots[count]];
+        // Set empty spot to current player
+        newBoard[availableSpots[count]] = player;
+        /*
+         Collect the score resulted from calling miniMax
+         on the opponent of the current player
+        */
+        if (player == ai.marker) {
+            let result = minimax(newBoard, human.marker);
+            move.score = result.score;
+        }
+        else {
+            let result = minimax(newBoard, ai.marker);
+            move.score = result.score;
+        }
+        // Reset spot to empty
+        newBoard[availableSpots[count]] = move.index;
+        // Push the object to the array
+        moves.push(move);
+    }
+    // If its the computer's turn loop over the moves and choose
+    let bestMove;
+    if (player === ai.marker) {
+        let bestScore = -10000;
+        for (let ix = 0; ix < moves.length; ix += 1) {
+            if (moves[ix].score > bestScore) {
+                bestScore = moves[ix].score;
+                bestMove = ix;
+            }
+        }
+    }
+    else {
+        // Else loop over the moves and choose the move with the lowest score
+        let bestScore = 10000;
+        for (let jx = 0; jx < moves.length; jx += 1) {
+            if (moves[jx].score < bestScore) {
+                bestScore = moves[jx].score;
+                bestMove = jx;
+            }
+        }
+    }
+    // Return the chosen object from the moves array
+    return moves[bestMove];
 }
+// Returns list of the indices of empty spots on the board
 function emptyIndices(board) {
     return board.filter(s => typeof (s) == 'number');
 }
+// Winning combinations using the board indices
 function winningCombo(board, player) {
-    if (threeAcross || threeDown || threeDiagonal && player.marker) {
+    if ((board[0] == player && board[1] == player && board[2] == player) ||
+        (board[3] == player && board[4] == player && board[5] == player) ||
+        (board[6] == player && board[7] == player && board[8] == player) ||
+        (board[0] == player && board[3] == player && board[6] == player) ||
+        (board[1] == player && board[4] == player && board[7] == player) ||
+        (board[2] == player && board[5] == player && board[8] == player) ||
+        (board[0] == player && board[4] == player && board[8] == player) ||
+        (board[2] == player && board[4] == player && board[6] == player)) {
         return true;
     }
     else {
--- tic-tac-toe/src/app.ts
+++ tic-tac-toe/src/app.ts
@@ -1,39 +1,113 @@
+interface MoveConfig {
+  index: number,
+  score: number
+}
+
 interface PlayerConfig {
-  marker: string;
-  score: number;
-  winsTotal: number;
+  marker: string,
+  winsTotal: number
 }
 
-let Player:object = {
-  marker: '',
-  score: 0,
-  winsTotal: 0
+let origBoard: any[] = ['O', 1, 'X', 'X', 4, 'X', 6, 'O', 'O'];
+
+let human:PlayerConfig = {
+  marker: 'O',
+  winsTotal: undefined
+};
+
+let ai:PlayerConfig = {
+  marker: 'X',
+  winsTotal: undefined
+};
+/* TEST
+let bestSpot = minimax(origBoard, ai.marker);
+console.log("index: " + bestSpot.index);  // 4
+*/
+function minimax(newBoard: any[], player: string) {
+  let availableSpots: number[] = emptyIndices(newBoard);
+
+  //check for terminal state
+  if (winningCombo(newBoard, human.marker)) {
+    return {score: -10};
+  } else if (winningCombo(newBoard, ai.marker)) {
+    return {score: 10};
+  } else if (availableSpots.length === 0) {
+    return {score: 0};
+  }
+
+  // An array to collect all the objects
+  let moves: any[] = [];
+
+  // Loop through available spots
+  for (let count: number = 0; count < availableSpots.length; count += 1) {
+    // Create an object for each and store the index of that spot
+    let move:MoveConfig = {
+      index: undefined,
+      score: undefined
+    };
+    move.index = newBoard[availableSpots[count]];
+
+    // Set empty spot to current player
+    newBoard[availableSpots[count]] = player;
+
+    /*
+     Collect the score resulted from calling miniMax
+     on the opponent of the current player
+    */
+    if (player == ai.marker) {
+      let result = minimax(newBoard, human.marker);
+      move.score = result.score;
+    } else {
+      let result = minimax(newBoard, ai.marker);
+      move.score = result.score;
+    }
+
+    // Reset spot to empty
+    newBoard[availableSpots[count]] = move.index;
+
+    // Push the object to the array
+    moves.push(move);
+  }
+
+  // If its the computer's turn loop over the moves and choose
+  let bestMove: number;
+  if (player === ai.marker) {
+    let bestScore: number = -10000;
+    for (let ix: number = 0; ix < moves.length; ix += 1) {
+      if (moves[ix].score > bestScore) {
+        bestScore = moves[ix].score;
+        bestMove = ix;
+      }
+    }
+  } else {
+  // Else loop over the moves and choose the move with the lowest score
+    let bestScore: number = 10000;
+    for (let jx: number = 0; jx < moves.length; jx += 1) {
+      if (moves[jx].score < bestScore){
+        bestScore = moves[jx].score;
+        bestMove = jx;
+      }
+    }
+  }
+
+  // Return the chosen object from the moves array
+  return moves[bestMove];
 }
-
-let origBoard:[string, number] = ['O', 1, 'X', 'X', 4, 'X', 6, 'O', 'O'];
-
-// Possible winning combinations
-const threeAcross = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
-const threeDown = [[0, 3 ,6], [1, 4, 7], [2, 5, 8]];
-const threeDiagonal = [[0, 4, 8], [2, 4, 6]];
-
-let human = Object.create(Player);
-human.marker = 'O';
-
-let ai = Object.create(Player);
-ai.marker = "X";
-
-// TODO: Build this function out
-function miniMax(newBoard: [string, number], player: PlayerConfig) {
-
-}
-
-function emptyIndices(board: [string, number]) {
+// Returns list of the indices of empty spots on the board
+function emptyIndices(board: any[]) {
   return board.filter(s => typeof(s) == 'number');
 }
-
-function winningCombo(board: [string, number], player: PlayerConfig) {
-  if (threeAcross || threeDown || threeDiagonal && player.marker) {
+// Winning combinations using the board indices
+function winningCombo(board: any[], player: string) {
+  if ((board[0] == player && board[1] == player && board[2] == player) ||
+   (board[3] == player && board[4] == player && board[5] == player) ||
+   (board[6] == player && board[7] == player && board[8] == player) ||
+   (board[0] == player && board[3] == player && board[6] == player) ||
+   (board[1] == player && board[4] == player && board[7] == player) ||
+   (board[2] == player && board[5] == player && board[8] == player) ||
+   (board[0] == player && board[4] == player && board[8] == player) ||
+   (board[2] == player && board[4] == player && board[6] == player)
+   ) {
     return true;
   } else {
     return false;
