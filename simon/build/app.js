a0a6790034946233aba9403688699fec9aefaaaa
Miguel T Rivera <mrivera108@outlook.com>
2017-06-14 23:54:20 -0700

    addEventListener to simon

--- simon/build/app.js
+++ simon/build/app.js
@@ -1,1 +1,18 @@
-//      
+'use strict';
+let simon = document.getElementById('simon');
+simon.addEventListener('click', playSimon, false);
+// Main simon gameplay
+function playSimon(e) {
+    const WIN = 20;
+    let pattern = [];
+}
+// Get the URL of the soundfile for the matching color
+function getSoundURL(soundIndex) {
+    return `https://s3.amazonaws.com/freecodecamp/simonSound${soundIndex}.mp3`;
+}
+// Get a random integer between 1 and 4
+function getRandomInt() {
+    const min = Math.ceil(1);
+    const max = Math.floor(5);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
--- simon/src/app.ts
+++ simon/src/app.ts
@@ -1,1 +1,22 @@
-// @flow
+'use strict';
+
+let simon = document.getElementById('simon');
+simon.addEventListener('click', playSimon, false);
+
+// Main simon gameplay
+function playSimon(e: any) {
+  const WIN = 20;
+  let pattern = [];
+}
+
+// Get the URL of the soundfile for the matching color
+function getSoundURL(soundIndex: number) {
+  return `https://s3.amazonaws.com/freecodecamp/simonSound${soundIndex}.mp3`;
+}
+
+// Get a random integer between 1 and 4
+function getRandomInt() {
+  const min = Math.ceil(1);
+  const max = Math.floor(5);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
