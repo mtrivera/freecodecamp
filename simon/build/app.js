c4b5d9c2b1ea4ca79a1daad968fc6ce934adf7e1
Miguel T Rivera <mrivera108@outlook.com>
2017-06-22 23:49:20 -0700

    Add control logic for click events

--- simon/build/app.js
+++ simon/build/app.js
@@ -4,7 +4,14 @@ simon.addEventListener('click', playSimon, false);
 // Main simon gameplay
 function playSimon(e) {
     const WIN = 20;
+    let controls = {
+        power: undefined,
+        strict: undefined,
+        start: undefined
+    };
+    isPower(controls);
     let pattern = [];
+    let patternObj = {};
 }
 // Get the URL of the soundfile for the matching color
 function getSoundURL(soundIndex) {
@@ -15,4 +22,32 @@ function getRandomInt() {
     const min = Math.ceil(1);
     const max = Math.floor(5);
     return Math.floor(Math.random() * (max - min)) + min;
+}
+// Check if the power switch is on; if not, alert user to turn game on
+function isPower(control) {
+    if (control.power) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+// Check if the start button has been pressed; if so, start the game
+function isStart(control) {
+    if (control.start) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+// Prompt user to play the game every 30 seconds, disable after 2.5 minutes
+function promptUser() {
+    const delay = 30000; // 30s
+    let timer = setInterval(alertUser, delay);
+    return timer;
+}
+// Message to user about playing the game
+function alertUser() {
+    alert(`Please turn power ON and click START to play`);
 }
--- simon/src/app.ts
+++ simon/src/app.ts
@@ -20,7 +20,15 @@ simon.addEventListener('click', playSimon, false);
 // Main simon gameplay
 function playSimon(e: any) {
   const WIN = 20;
+  let controls:ControlsConfig = {
+    power: undefined,
+    strict: undefined,
+    start: undefined
+  };
+  isPower(controls);
+
   let pattern = [];
+  let patternObj:PatternConfig = {};
 }
 
 // Get the URL of the soundfile for the matching color
@@ -33,4 +41,30 @@ function getRandomInt() {
   const min = Math.ceil(1);
   const max = Math.floor(5);
   return Math.floor(Math.random() * (max - min)) + min;
+}
+// Check if the power switch is on; if not, alert user to turn game on
+function isPower(control: ControlsConfig) {
+  if (control.power) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// Check if the start button has been pressed; if so, start the game
+function isStart(control: ControlsConfig) {
+  if (control.start) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// Prompt user to play the game every 30 seconds, disable after 2.5 minutes
+function promptUser() {
+  const delay = 30000;  // 30s
+  let timer = setInterval(alertUser, delay);
+  return timer;
+}
+// Message to user about playing the game
+function alertUser() {
+  alert(`Please turn power ON and click START to play`);
 }
