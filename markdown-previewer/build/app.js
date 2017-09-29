6887d12ec415a1c692ac7300320b5e0fdbc4b2a3
Miguel T Rivera <mrivera108@outlook.com>
2017-09-28 20:46:16 -0700

    Create Vue instance

---
+++ markdown-previewer/build/app.js
@@ -0,0 +1,12 @@
+const mdapp = new Vue({
+  el: '#app',
+  data: {
+    mdInput: '' /*,
+                methods: {
+                 change: function() {
+                   marked(this.mdInput)
+                 }
+                }*/
+  } });
+
+//mdapp.change();
---
+++ markdown-previewer/src/app.js
@@ -0,0 +1,13 @@
+const mdapp = new Vue({
+  el: '#app',
+  data: {
+    mdInput: ''
+  }/*,
+  methods: {
+    convertToMarkdown: function() {
+      marked(this.mdInput)
+    }
+  }*/
+})
+
+//mdapp.convertToMarkdown();
